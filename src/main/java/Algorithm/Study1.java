e11047041ae58256c8238f54ad683bd6785d3213
xiaohao01 <xiaohao01@qiyi.com>
2019-11-04 19:47:17 +0800

    1

---
+++ src/main/java/Algorithm/Study1.java
@@ -0,0 +1,14 @@
+package Algorithm;
+
+/**
+ * Created by xiaohao on 2019-11-04
+ **/
+public class Study1 {
+    public void test() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
